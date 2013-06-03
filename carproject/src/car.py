a263952e64369123225f40909cedb3cae2c24a0a
rodsur <rodsur@rodsur.net>
2013-06-03 20:45:42 +0200

    Car should be able to accelerate and break according to set speed limit and distance to next car in line

--- carproject/src/car.py
+++ carproject/src/car.py
@@ -1,14 +1,85 @@
 class car(object):
     speed = 0
-    maxbreakstr = 0.10
     position = 0
     accel = 0
     maxspeed = 100
-    def __init__(self,speed=0, maxbreakstr=0.10, position=0):
+    mode = "automatic"
+    alloweddistance = 20
+    nextCar = None
+    speedAim = 0
+    def __init__(self,speed=0, position=0,maxspeed=100,mode="automatic"):
         self.speed = speed
-        self.maxbreakstr = maxbreakstr
         self.position = position
-        pass
+        self.maxspeed = maxspeed
+        self.setMode(mode)
+    
+    def setMode(self,mode):
+        if mode.lower() != "automatic" and mode.lower() != "override":
+            print(mode.lower())
+            print("Mode not set properly, assuming automatic mode")
+            self.mode = "automatic"
+        else:
+            self.mode = mode.lower()
+    
     def getSpeed(self):
         return self.speed
     
+    def getAccel(self):
+        return self.accel
+    
+    def getPosition(self):
+        return self.position
+    
+    def getMaxSpeed(self):
+        return self.maxspeed
+    
+    def getDistanceForward(self, othercar):
+        if othercar == None:
+            return 0
+        else:
+            distance = othercar.getPosition() - self.getPosition()
+            return distance
+    
+    def getDistanceBehind(self, othercar):
+        distance = self.getPosition() - othercar.getPosition()
+        return distance
+    
+    def setNextCar(self,nextCar):
+        self.nextCar = nextCar
+    
+    def setSpeed(self, speed):
+        if speed != 0:
+            self.speed = speed
+        else:
+            self.speed = self.getSpeed() + self.getAccel()
+    
+    def setAccel(self, accel):
+        self.accel = accel
+        
+    def setSpeedAim(self,SpeedAim):
+        self.speedAim = SpeedAim
+    
+    def doDrive(self):
+        self.position = self.getPosition() + self.getSpeed()
+        
+    def doAccel(self):
+        self.speed = self.speed + self.accel
+            
+    def act(self):
+        if self.mode == "automatic":
+            if self.getDistanceForward(self.nextCar) < self.alloweddistance:
+                self.setAccel(-2)
+            elif self.speed < self.speedAim:
+                self.doAccel()
+                self.doDrive()
+            elif self.speed == self.speedAim:
+                self.setAccel(0)
+                self.doDrive()
+            pass
+        elif self.mode == "override":
+            self.doDrive()
+            pass
+        pass
+        
+if __name__ == "__main__":
+    pass
--- carproject/src/cartest.py
+++ carproject/src/cartest.py
@@ -9,11 +9,54 @@ import car
 class Test(unittest.TestCase):
 
 
-    def testCar(self):
+    def testCarStart(self): #tests initilization of cars and checks variables
+        print("Starting start test")
         car1 = car.car()
+        car2 = car.car(position=20)
+        car1.setNextCar(car2)
         Test.assertEqual(self, 0, car1.getSpeed(), "speed of car 1 does not equal 0")
-        pass
-
+        Test.assertEqual(self, 0, car1.getPosition(), "Position of car 1 does not equal 0")
+        Test.assertEqual(self, 0, car1.getAccel(), "Acceleration of car 1 does not equal 0")
+        Test.assertEqual(self, 100, car1.getMaxSpeed(), "Max Speed of car 1 does not equal 100")
+        Test.assertEqual(self, 20, car1.getDistanceForward(car2), "Distance between cars is not 20")
+        Test.assertEqual(self, 20, car2.getDistanceBehind(car1), "Distance from car 2 to car 1 is not 20")
+    
+    def testCarSpeed(self): #should check speed
+        print("Starting speed test")
+        car1 = car.car(mode="override")
+        car1.setSpeed(20)
+        Test.assertEqual(self, 0, car1.getPosition(), "Car 1 was not where assumed")
+        Test.assertEqual(self, 20, car1.getSpeed(), "Car 1 is not driving with assumed speed")
+        car1.act()
+        Test.assertEqual(self, 20, car1.getPosition(), "Car 1 was still not where assumed")
+        car1.setSpeed(30)
+        car1.act()
+        Test.assertEqual(self, 50, car1.getPosition(), "Car 1 was not where assumed")
+    
+    def testCarAccel(self):
+        print("Starting acceleration test")
+        car1 = car.car(mode="automatic")
+        car1.setAccel(2)
+        Test.assertEqual(self, 0, car1.getPosition(), "car 1 was not at 0")
+        car1.setSpeedAim(50)
+        car1.act()
+        Test.assertEqual(self, 2, car1.getPosition(), "Car 1 was not at 2")
+        Test.assertEqual(self, 2, car1.getSpeed(), "Car did not have a speed of 2")
+        car1.act()
+        Test.assertEqual(self, 6, car1.getPosition(), "Car was not at 6")
+        Test.assertEqual(self, 4, car1.getSpeed(), "Car did was not at speed 4")
+        count = 0
+        while (count < 25):
+            car1.act()
+            count = count + 1
+        Test.assertEqual(self, 750, car1.getPosition(), "car was not at expected position")
+        Test.assertEqual(self, 50, car1.getSpeed(), "car has either crossed or not reached speed limit")
+        car1.act()
+        Test.assertEqual(self, 800, car1.getPosition(), "car has not moved after achieving max speed")
+    
+    def testKeepDistance(self):
+        print("Starting acceleration test")
+        
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testCar']
